f2870554cf36304df96c32d22a291d6fd7f32fc8
Alex <arojasmarulanda@gmail.com>
2021-11-04 12:52:14 -0500

    Python test-execution completed

---
+++ test.py
@@ -0,0 +1,22 @@
+from speedtest import Speedtest
+from datetime import datetime
+
+
+class Test:
+    def __init__(self):
+        self.__s=Speedtest()
+        self.__data=[]
+
+    def TestExecution(self):
+        self.__data.append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        self.__data.append(round(self.__s.download()/1048576,2))
+        self.__data.append(round(self.__s.upload()/1048576,2))
+        self.__data.append(self.__s.get_best_server()['name'])
+        self.__data.append(self.__s.get_best_server()['country'])
+        self.__data.append(self.__s.get_best_server()['sponsor'])
+        self.__data.append(self.__s.get_best_server()['latency'])
+        return self.__data
+
+t=Test()
+
+#print(t.TestExecution())
